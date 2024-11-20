3fbd2f71b20ab6c410a7e18311554ad21ea22813
csongph <67030052@kmitl.ac.th>
2024-11-20 15:35:00 +0700

    Update multiplication_table.py

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,5 +1,5 @@
 #รับข้อมูลตัวเลข
 n = int(input("Enter a number:\n"))
 #นำตัวเลขที่ป้อนไปคูณกับตัวเลขเป็นสูตรคูณที่ต้องการ
-for i in range(10):  
+for i in range(0,10):  
     print(f"{i} x {n} = {i * n}")
