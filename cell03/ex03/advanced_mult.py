6df11d6638f9036f4e16e5b77626c540f472fbf6
csongph <67030052@kmitl.ac.th>
2024-11-20 17:38:42 +0700

    Update advanced_mult.py

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -2,7 +2,7 @@ i = 0
 while i <= 10: 
     print(f"Table de {i}: ", end="")
     x = 0
-    while x <= 10:  # คูณตั้งแต่ 0 ถึง 10
+    while x <= 10:  
         print(f"{i * x} ", end="")
         x += 1 
     print()
