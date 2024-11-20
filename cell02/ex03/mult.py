6e232ff5bf1ade6430536227658123c80aa71f25
csongph <67030052@kmitl.ac.th>
2024-11-20 09:02:11 +0700

    Update mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,1 +1,13 @@
+umber1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
 
+result = number1 * number2
+
+if result > 0:
+    print(f"The result is positive.")
+elif result < 0:
+    print(f"The result is negative.")
+else:
+    print(f"The result is positive and negative.")
+
+print(f"{number1} * {number2} = {result}")
