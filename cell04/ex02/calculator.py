833efbee268f266e3b897f266a3f6e495bcc3ef5
csongph <67030052@kmitl.ac.th>
2024-11-21 08:55:50 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,7 @@
+n1 = int(input("give me the first number: "))
+n2 = int(input("give me the second number: "))
+print(F"thank you")
+print(f" {n1} + {n2} = {n1 + n2}")
+print(f" {n1} - {n2} = {n1 - n2}")
+print(f" {n1} / {n2} = {n1 / n2}")
+print(f" {n1} * {n2} = {n1 * n2}")
